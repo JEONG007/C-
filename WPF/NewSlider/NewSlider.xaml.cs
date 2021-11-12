7051158831d7bc6941182266161b9d7562e06367
Jeong <phjeong6146@gmail.com>
2021-11-12 17:10:44 +0900

    NewSlider 수정

--- WPF/NewSlider/NewSlider.xaml.cs
+++ WPF/NewSlider/NewSlider.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,34 +12,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-namespace NewSlider
+namespace Slider
 {
     /// <summary>
     /// NewSlider.xaml에 대한 상호 작용 논리
     /// </summary>
+
     public partial class NewSlider : UserControl, INotifyPropertyChanged
     {
         private bool _isMove = false;
         private String _sValue = "0";
+        private int _width = 0;
+        private int _max = 100;
         private Point ptStartMove;
         private Thickness mgnStartMove;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged; 
 
-        public NewSlider()
-        {
-            InitializeComponent();
-        }
-        public System.Windows.Media.Brush Fill
-        {
+        public System.Windows.Media.Brush Fill { //슬라이더 버튼 색상
             set
             {
                 ybar.Fill = value;
             }
         }
-        public String Value
-        {
+
+        public String Value //슬라이더 값
+        { 
             get => _sValue;
             set
             {
@@ -50,9 +49,20 @@ namespace NewSlider
             }
         }
 
+        public int Max { get => _max; set => _max = value; }
+
+        public NewSlider()
+        {
+            InitializeComponent();
+        }
+
         public void OnPropertyChanged([CallerMemberName] string name = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private double GetPercentage(double value)
+        {
+            return System.Math.Round(value * 100 / Max, 100);
         }
 
         private void ybar_MouseMove(object sender, MouseEventArgs e)
@@ -61,28 +71,32 @@ namespace NewSlider
 
             Point pt = Mouse.GetPosition(this);
             int moveX = (int)(mgnStartMove.Left + (pt.X - ptStartMove.X));
-            if (moveX >= 0 && moveX <= 300)
+            if(moveX >= 0 && moveX <= _width)
             {
                 ybar.Margin = new Thickness(moveX, 0, 0, 0);
                 //text.Text = ((int)moveX).ToString();
                 Value = moveX.ToString();
             }
         }
+
         private void ybar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             _isMove = true;
+            _width = (int)xbar.ActualWidth;
             ptStartMove = e.GetPosition(this);
             mgnStartMove = ybar.Margin;
+            ybar.CaptureMouse(); //영영밖에서도 스크롤 유지
         }
 
         private void ybar_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             _isMove = false;
+            ybar.ReleaseMouseCapture();
         }
 
-        private void ybar_MouseLeave(object sender, MouseEventArgs e)
-        {
-            _isMove = false;
-        }
+        //private void ybar_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    _isMove = false;
+        //}
     }
 }
